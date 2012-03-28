d03af5b7956f1cb8f8f255a080baa34c6886e5d4
Shilov Vlad <Omgovich@ya.ru>
2012-03-29 00:54:22 +0400

    Add sources

---
+++ sources/jquery.splitter.js
@@ -0,0 +1,25 @@
+/*
+ * jQuery Splitter
+ *
+ * Copyright (c) 2011 Shilov Vlad [Omgovich] (http://omgovich.ru/)
+ * 
+ */
+
+(function($) {
+
+	$.fn.splitter = function(options) {
+	
+		// Settings
+		var settings = $.extend({
+			//
+		}, options);
+	
+		// Install splitter for each items
+		this.each(function(){
+			//
+		});
+	
+		return this;
+	};
+
+})(jQuery);
