6300e673df2f19480ffef84cf960bf984070b50d
Shilov Vlad <Omgovich@ya.ru>
2012-03-29 20:12:25 +0400

    First version of code and simple demo

--- sources/jquery.splitter.js
+++ sources/jquery.splitter.js
@@ -11,12 +11,43 @@
 	
 		// Settings
 		var settings = $.extend({
-			//
+			columns: 2,
+			itemsSelector: 'li',
+			// container
+			containerTag: 'div',
+			containerClass: 'list-container',
+			// column
+			columnTag: 'ul',
+			columnClass: 'list-column',
+			columnFirstClass: 'list-column_first',
+			columnLastClass: 'list-column_last'
 		}, options);
 	
 		// Install splitter for each items
 		this.each(function(){
-			//
+			
+			var $list = $(this),
+			    $items = $list.find(settings.itemsSelector),
+				itemsInColumn = Math.ceil($items.length/settings.columns),
+			    $container = $('<'+settings.containerTag+'/>', {
+					class: settings.containerClass
+				});
+
+			for(var i=0; i<$items.length; i+=itemsInColumn) {
+				var $columnItems = $items.slice(i, i+itemsInColumn).clone(),
+					$column = $('<'+settings.columnTag+'/>', {
+						class: settings.columnClass
+					});
+				$column.append($columnItems);
+				$container.append($column);
+			};
+			
+			$container.find('> :first').addClass(settings.columnFirstClass);
+			$container.find('> :last').addClass(settings.columnLastClass);
+			
+			$list.after($container);
+
+			
 		});
 	
 		return this;
