109f0ef37605993ab3cdfcd68c4bc6e1f801d670
Shilov Vlad <omgovich@ya.ru>
2013-12-03 00:56:55 +0400

    Update code, fixes, "columnAllowEmpty" option

--- jquery.splitter.js
+++ jquery.splitter.js
@@ -1,26 +1,31 @@
 /*
- * jQuery Splitter
+ * jQuery Splitter v1.1
  * Copyright (c) 2013 Shilov Vlad [Omgovich] (http://omgovich.ru/)
  */
 
 (function($) {
-
-	$.fn.splitter = function(options) {
 	
+	// Settings
+	var defaults = {
+		columns: 2,
+		direction: 'vertical',
+		itemsSelector: 'li',
+		// container
+		containerTag: 'div',
+		containerClass: 'list-container',
+		// column
+		columnTag: 'ul',
+		columnClass: 'list-column',
+		columnFirstClass: 'list-column_first',
+		columnLastClass: 'list-column_last',
+		columnAllowEmpty: false
+	};
+	
+	// Splitter function
+	$.fn.splitter = function(options) {
+
 		// Settings
-		var settings = $.extend({
-			columns: 2,
-			direction: 'vertical',
-			itemsSelector: 'li',
-			// container
-			containerTag: 'div',
-			containerClass: 'list-container',
-			// column
-			columnTag: 'ul',
-			columnClass: 'list-column',
-			columnFirstClass: 'list-column_first',
-			columnLastClass: 'list-column_last'
-		}, options);
+		var settings = $.extend(defaults, options);
 	
 		// Install splitter for each items
 		this.each(function(){
@@ -34,19 +39,19 @@
 			    }),
 			    $container = $('<'+settings.containerTag+'/>', {
 			    	'class': settings.containerClass
-			    });
+			    }),
+			    $columns;
 
-			// Create columns
-			for(var i=0; i<settings.columns; i++) $container.append( $column.clone() );
-			var $columns = $container.children();
-			$columns.first().addClass(settings.columnFirstClass);
-			$columns.last().addClass(settings.columnLastClass);
+			// Find coolumns
+			function findColumns(){
+				$columns = $container.children();
+			};
 
 			// Vertical split
 			function splitVertical(){
 				var itemsInColumn = Math.ceil(itemsNumber/settings.columns),
 				    column = 0;
-				for(var i=0; i<itemsNumber; i+=itemsInColumn) {
+				for (var i=0; i<itemsNumber; i+=itemsInColumn) {
 					var $columnItems = $items.slice(i, i+itemsInColumn).clone();
 					$columns.eq(column++).append($columnItems);
 				};
@@ -59,14 +64,28 @@
 				});
 			};
 
+			// Create columns
+			for (var i=0; i<settings.columns; i++) $container.append($column.clone());
+			findColumns();
+
 			// Add items in columns
 			if (settings.direction=='horizontal') splitHorizontal(); else splitVertical();
+
+			// Remove empty columns
+			if (!settings.columnAllowEmpty) {
+				$columns.filter(':empty').remove();
+				findColumns();
+			};
+
+			// Add classes to first and last columns
+			$columns.first().addClass(settings.columnFirstClass);
+			$columns.last().addClass(settings.columnLastClass);
 
 			// Render splitted list
 			$list.replaceWith($container);
 
 		});
-	
+
 		return this;
 	};
 
